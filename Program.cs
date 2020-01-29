37c5855b58d5c58af87685bf58ad7148582f841a
Matias Moreira <moreiramatias10@hotmail.com>
2020-01-29 04:02:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ruleta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ruleta = new Ruleta();
+            int numero = 0, numWin, winners, losers;
+            while (numero >= 0 && numero <= 36)
+            {
+                Console.Write("Ingresar el numero al que desea apostar: ");
+                numero = int.Parse(Console.ReadLine());
+                if (numero >= 0 && numero <= 36)
+                {
+                    ruleta.addBets(numero);
+                }
+            }
+            numWin = ruleta.getNumberWin();
+            winners = ruleta.getResults(numWin);
+            losers = ruleta.getAllBets().Count - winners;
+            Console.WriteLine("El numero ganador es el " + numWin.ToString());
+            Console.WriteLine("La cantidad de ganadores es de: " + ruleta.getResults(numWin).ToString());
+            Console.WriteLine("La cantidad de perdedores es de: " + losers.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ruleta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ruleta
+{
+    class Ruleta
+    {
+        private List<int> _bets;
+        public Ruleta()
+        {
+            _bets = new List<int>();
+        }
+        public void addBets(int value)
+        {
+            _bets.Add(value);
+        }
+        public List<int> getAllBets()
+        {
+            return _bets;
+        }
+        public int getNumberWin()
+        {
+            var rand = new Random();
+            return rand.Next(0, 36);
+        }
+        public int getResults(int numberWin)
+        {
+            int winners = 0;
+            foreach (var bet in _bets)
+            {
+                if (bet == numberWin)
+                {
+                    winners++;
+                }
+            }
+            return winners;
+        }
+    }
+}
